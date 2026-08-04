21d87c0c198a9e3de8703be619f26270e0b58fad
grace-sampao <gnsampao@gmail.com>
2026-08-04 20:26:03 +0300

    Updated agent/agent.py.

--- agent/agent.py
+++ agent/agent.py
@@ -1,4 +1,4 @@
-import config.environment
+import config.environment, prompts
 import importlib
 
 importlib.reload(config.environment)
@@ -7,5 +7,7 @@ from langchain.agents import create_agent
 
 agent = create_agent(
   model="deepseek:deepseek-v4-flash",
-  api_key=config.environment.DEEPSEEK_API_KEY
+  api_key=config.environment.DEEPSEEK_API_KEY,
+  tools=[],
+  system_prompt=prompts.system_message
 )
