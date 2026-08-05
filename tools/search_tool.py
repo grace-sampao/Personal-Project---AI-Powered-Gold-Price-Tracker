cc243a19fbe9830d530fa3ef955f0c74a42d75ca
grace-sampao <gnsampao@gmail.com>
2026-08-05 08:54:15 +0300

    First commit.

---
+++ tools/search_tool.py
@@ -0,0 +1,19 @@
+import sys
+from pathlib import Path
+import importlib
+
+root_dir = Path(__file__).resolve().parent.parent
+sys.path.append(str(root_dir))
+
+from config import environment
+importlib.reload(environment)
+
+from langchain_tavily import TavilySearch
+
+search_tool = TavilySearch(
+  max_results=5,
+  tavily_api_key=environment.TAVILY_API_KEY,
+  topic="finance",
+  time_range="week",
+  # include_domains=[]
+)
