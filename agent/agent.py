f3b44974f38934451a6e9f22b48865407839f613
grace-sampao <gnsampao@gmail.com>
2026-08-05 09:27:08 +0300

    Updated agent/agent.py.

--- agent/agent.py
+++ agent/agent.py
@@ -1,13 +1,48 @@
-import config.environment, prompts
+import sys
+from pathlib import Path
 import importlib
 
-importlib.reload(config.environment)
+root_dir = Path(__file__).resolve().parent.parent
+sys.path.append(str(root_dir))
+
+from tools import search_tool
+from config import environment
+import prompts
+
+importlib.reload(environment)
 
 from langchain.agents import create_agent
+from langchain_deepseek import ChatDeepSeek
+
+model = ChatDeepSeek(
+  model="deepseek-v4-flash",
+  api_key=environment.DEEPSEEK_API_KEY,
+  temperature=0,
+  max_tokens=None,
+  timeout=None,
+  max_retries=2
+)
 
 agent = create_agent(
-  model="deepseek:deepseek-v4-flash",
-  api_key=config.environment.DEEPSEEK_API_KEY,
-  tools=[],
-  system_prompt=prompts.system_message
+  model=model,
+  tools=[]
+)
+
+response = agent.invoke(
+  {
+    "messages": [
+      {
+        "role": "system",
+        "content": "You are a poetry expert"
+      },
+      {
+        "role": "user",
+        "content": "Write a haiku about spring"
+      }
+    ]
+  }
+)
+
+print(
+  response['messages'][-1].content_blocks[-1]['text']
 )
