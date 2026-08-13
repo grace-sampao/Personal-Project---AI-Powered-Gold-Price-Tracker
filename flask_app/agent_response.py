b884d44f0821057ce80b3449418f385c6b566471
grace-sampao <gnsampao@gmail.com>
2026-08-13 15:27:54 +0300

    Updated flask_app/agent_response.py.

--- flask_app/agent_response.py
+++ flask_app/agent_response.py
@@ -9,6 +9,7 @@ sys.path.append(
 
 from flask import Blueprint, render_template
 from agent.agent import invoke_agent
+import markdown
 
 
 bp = Blueprint(
@@ -17,7 +18,10 @@ bp = Blueprint(
 
 @bp.route("/", methods=["GET"])
 def homepage():
-  response = invoke_agent()
+  markdown_response = invoke_agent()
+  response = markdown.markdown(
+    markdown_response, extensions=["extra", "codehilite"]
+  )
 
   return render_template(
     "index.html", gold_price_summary=response
