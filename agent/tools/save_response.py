3b8c7c49d710278368a4c24b962787adc9e3de67
grace-sampao <gnsampao@gmail.com>
2026-09-01 13:35:15 +0300

    Updated agent/tools/save_response.py.

--- agent/tools/save_response.py
+++ agent/tools/save_response.py
@@ -2,11 +2,14 @@ from langchain.tools import tool, ToolRuntime
 
 
 @tool
-def save_response(response: str, runtime: ToolRuntime) -> str:
+def save_response(
+  date: str,
+  summary: dict[str, str],
+  runtime: ToolRuntime) -> str:
   """
   Save the agent response into the memory.
   """
   store = runtime.store
-  store.put(("responses",), response)
+  store.put(("responses",), date, summary)
 
   return "Sucessfully saved agent response."
