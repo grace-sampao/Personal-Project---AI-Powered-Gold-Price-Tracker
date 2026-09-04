eee8be67b4d3b2b094c7565c6d03f14cae63cc6a
grace-sampao <gnsampao@gmail.com>
2026-09-04 18:05:11 +0300

    Updated agent/tools/search_tool.py.

--- agent/tools/search_tool.py
+++ agent/tools/search_tool.py
@@ -1,22 +1,24 @@
-import sys
-from pathlib import Path
-import importlib
-
-root_dir = Path(__file__).resolve().parent.parent
-sys.path.append(
-  str(root_dir)
-)
-
-from config import config
-importlib.reload(config)
-
+from langchain.tools import tool
 from langchain_tavily import TavilySearch
+from agent.config import config
 
 
 tavily_search = TavilySearch(
   max_results=5,
   tavily_api_key=config.TAVILY_API_KEY,
   topic="finance",
-  time_range="week",
-  # include_domains=[],
+  time_range="week"
 )
+
+@tool
+def search_web(query: str) -> str:
+  """
+  Searches the web for the latest financial and geopolitical news and commodity prices.
+  Use this tool to get information relevant to the analysis.
+  """
+  try:
+    results = tavily_search.run(query)
+
+    return results
+  except Exception as e:
+    return f"Error during search {str(e)}"
